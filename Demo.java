c292daed31f2b4e9534a422920af8ac6f82294d8
yogesh220 <69450013+yogesh220@users.noreply.github.com>
2021-06-18 18:46:38 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,28 @@
+public class Demo
+{
+
+
+public void m1()
+{
+
+System.out.println("hi");
+
+}
+
+
+
+public static void main(String args[])
+
+{
+
+
+Demo d=new Demo();
+
+
+d.m1();
+system.out.println("hiiiiiiiiii");
+
+}
+
+
+}
